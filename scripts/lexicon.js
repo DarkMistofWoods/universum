27f410b31ef100272f17a3cbd3dd4e155ca07dd9
lucasemerson <zetleader@gmail.com>
2024-03-25 19:03:19 -0700

    Update lexicon.js

--- scripts/lexicon.js
+++ scripts/lexicon.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    fetch('resources/terms.txt')
+    fetch('universim/resources/terms.txt')
         .then(response => response.text())
         .then(text => {
             lexiconData = text.split('\n').map(line => {
