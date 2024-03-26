0fc51dedff43781550b4a8909c6170ae3336d4bc
lucasemerson <zetleader@gmail.com>
2024-03-25 19:20:30 -0700

    Update lexicon.js

--- scripts/lexicon.js
+++ scripts/lexicon.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    fetch('terms.txt')
+    fetch('https://darkmistofwoods.github.io/universum/resources/terms.txt')
         .then(response => response.text())
         .then(text => {
             lexiconData = text.split('\n').map(line => {
