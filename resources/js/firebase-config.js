fa09888397815213ffbdc9942d3a98d8d4ea7a73
darkmistofwoods <zetleader@gmail.com>
2024-04-19 11:00:06 -0700

    Update firebase-config.js

--- resources/js/firebase-config.js
+++ resources/js/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions needed from the SDKs
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js';
-import { getFirestore, doc, getDoc, getDocs, addDoc, updateDoc, collection, deleteDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js';
+import { getFirestore, doc, getDoc, getDocs, addDoc, updateDoc, collection, deleteDoc, serverTimestamp, query, where } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -239,4 +239,4 @@ export {
     getDocs,
     deleteDoc,
     serverTimestamp
-  };
+};
