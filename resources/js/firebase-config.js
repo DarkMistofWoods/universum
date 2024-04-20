bc7bd974f532ec7d5deaf2b14f231780383fba92
darkmistofwoods <zetleader@gmail.com>
2024-04-20 08:48:49 -0700

    Update firebase-config.js

--- resources/js/firebase-config.js
+++ resources/js/firebase-config.js
@@ -219,27 +219,29 @@ async function fetchUserProfile(userId) {
 }
 
 async function addGoal(userId, goalType, goalAmount) {
-  const userGoalsRef = collection(db, 'users', userId, 'goals');
-  const userGoalsSnapshot = await getDocs(userGoalsRef);
+    const userGoalsRef = collection(db, 'users', userId, 'goals');
+    const userGoalsSnapshot = await getDocs(userGoalsRef);
 
-  if (userGoalsSnapshot.size < 3) {
-    const newGoalData = {
-      description: goalType,
-      targetDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-      target: goalAmount,
-      progress: 0,
-      lastUpdated: serverTimestamp()
-    };
+    if (userGoalsSnapshot.size < 3) {
+        const newGoalData = {
+            description: goalType,
+            targetDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+            target: goalAmount,
+            progress: 0,
+            lastUpdated: serverTimestamp()
+        };
 
-    await addDoc(userGoalsRef, newGoalData);
-  } else {
-    throw new Error('You have reached the maximum number of goals (3).');
-  }
+        await addDoc(userGoalsRef, newGoalData);
+        localStorage.setItem('userGoalsLastCachedTimestamp', new Date().getTime().toString());
+    } else {
+        throw new Error('You have reached the maximum number of goals (3).');
+    }
 }
 
 async function removeGoal(userId, goalId) {
-  const goalRef = doc(db, 'users', userId, 'goals', goalId);
-  await deleteDoc(goalRef);
+    const goalRef = doc(db, 'users', userId, 'goals', goalId);
+    await deleteDoc(goalRef);
+    localStorage.setItem('userGoalsLastCachedTimestamp', new Date().getTime().toString());
 }
 
 export {
