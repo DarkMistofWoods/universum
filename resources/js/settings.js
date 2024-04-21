6ff8975cf3d27fd5735cc4a9be3f4f57b9d5814e
darkmistofwoods <zetleader@gmail.com>
2024-04-21 10:53:01 -0700

    Update settings.js

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -57,7 +57,6 @@ async function updateEmail(userId, newEmail) {
 auth.onAuthStateChanged(async (user) => {
     if (user) {
         const settings = await loadSettings(user.uid);
-        console.log(document.querySelector(`[name="learning-path"][value="${settings.learningPath}"]`).checked);
 
         document.querySelector('[name="learning-pace"]').value = settings.learningPace;
         document.querySelectorAll('[name="content-pref"]').forEach(checkbox => {
