891718336e4779d4ca49ee4d4c1dc3c61896727c
darkmistofwoods <zetleader@gmail.com>
2024-04-24 11:59:01 -0700

    Update currentProgress.js
    Created a dedicated function to retrieve the overall progress including incomplete nodes

--- resources/js/currentProgress.js
+++ resources/js/currentProgress.js
@@ -7,90 +7,60 @@ async function initializeVisualization(user) {
 }
 
 async function createVisualization(courseContent, userProgress) {
-    const width = 800;
-    const height = 600;
-    const centerX = width / 2;
-    const centerY = height / 2;
+    const progress = calculateProgress(courseContent, userProgress);
+    console.log(progress);
+    // Create the visualization using the progress data
+}
 
-    const svg = d3.select('.container-tertiary')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
+function calculateProgress(courseContent, userProgress) {
+    const progressData = {
+        modules: {},
+        subModules: {},
+        lessons: userProgress,
+    };
 
-    const moduleGroups = svg.selectAll('.module')
-        .data(courseContent)
-        .enter()
-        .append('g')
-        .attr('class', 'module')
-        .attr('transform', (d, i) => `translate(${centerX + Math.cos(i * 2 * Math.PI / courseContent.length) * 200}, ${centerY + Math.sin(i * 2 * Math.PI / courseContent.length) * 200})`);
+    courseContent.forEach((module) => {
+        const moduleId = module.moduleId;
+        let moduleProgress = 0;
+        let moduleQuizScore = 0;
 
-    moduleGroups.append('text')
-        .attr('class', 'module-label')
-        .attr('text-anchor', 'middle')
-        .attr('dy', '1.5em')
-        .text(d => d.moduleName);
+        module.subModules.forEach((subModule) => {
+            const subModuleId = subModule.subModuleId;
+            let subModuleProgress = 0;
+            let subModuleQuizScore = 0;
 
-    const subModuleGroups = moduleGroups.selectAll('.submodule')
-        .data(d => d.subModules)
-        .enter()
-        .append('g')
-        .attr('class', 'submodule')
-        .attr('transform', (d, i, nodes) => {
-            const parentTransform = d3.select(nodes[i].parentNode).attr('transform');
-            const [x, y] = parentTransform.substring(parentTransform.indexOf('(') + 1, parentTransform.indexOf(')')).split(',');
-            const angle = i * 2 * Math.PI / nodes.length;
-            const radius = 100;
-            return `translate(${parseFloat(x) + Math.cos(angle) * radius}, ${parseFloat(y) + Math.sin(angle) * radius})`;
+            subModule.lessons.forEach((lesson) => {
+                const lessonId = lesson.lessonId;
+                const lessonProgress = userProgress[lessonId]?.completed ? 1 : 0;
+                const lessonQuizScore = userProgress[lessonId]?.quizScores?.reduce((sum, score) => sum + score, 0) || 0;
+                const lessonQuizCount = userProgress[lessonId]?.quizScores?.length || 0;
+
+                subModuleProgress += lessonProgress;
+                subModuleQuizScore += lessonQuizCount > 0 ? lessonQuizScore / lessonQuizCount : 0;
+            });
+
+            subModuleProgress /= subModule.lessons.length;
+            subModuleQuizScore /= subModule.lessons.length;
+
+            progressData.subModules[subModuleId] = {
+                progress: subModuleProgress,
+                quizScore: subModuleQuizScore,
+            };
+
+            moduleProgress += subModuleProgress;
+            moduleQuizScore += subModuleQuizScore;
         });
 
-    const lessonGroups = subModuleGroups.selectAll('.lesson')
-        .data(d => d.lessons)
-        .enter()
-        .append('g')
-        .attr('class', 'lesson')
-        .attr('transform', (d, i, nodes) => {
-            const parentTransform = d3.select(nodes[i].parentNode).attr('transform');
-            const [x, y] = parentTransform.substring(parentTransform.indexOf('(') + 1, parentTransform.indexOf(')')).split(',');
-            const angle = i * 2 * Math.PI / nodes.length;
-            const radius = 50;
-            return `translate(${parseFloat(x) + Math.cos(angle) * radius}, ${parseFloat(y) + Math.sin(angle) * radius})`;
-        });
+        moduleProgress /= module.subModules.length;
+        moduleQuizScore /= module.subModules.length;
 
-    lessonGroups.append('image')
-        .attr('xlink:href', './resources/icons/Universum-Sun-Icon.svg')
-        .attr('width', 20)
-        .attr('height', 20)
-        .attr('x', -10)
-        .attr('y', -10)
-        .attr('class', d => `lesson-star ${userProgress[d.lessonId]?.completed ? 'completed' : ''}`);
-
-    lessonGroups.append('text')
-        .attr('class', 'lesson-label')
-        .attr('text-anchor', 'middle')
-        .attr('dy', '2em')
-        .text(d => d.title)
-        .attr('visibility', 'hidden');
-
-    lessonGroups.on('mouseover', function () {
-        d3.select(this).select('.lesson-label').attr('visibility', 'visible');
-    }).on('mouseout', function () {
-        d3.select(this).select('.lesson-label').attr('visibility', 'hidden');
+        progressData.modules[moduleId] = {
+            progress: moduleProgress,
+            quizScore: moduleQuizScore,
+        };
     });
 
-    const paths = svg.selectAll('path')
-        .data(courseContent)
-        .enter()
-        .append('path')
-        .attr('class', 'constellation-path')
-        .attr('d', (d, i) => {
-            const source = d3.select(moduleGroups.nodes()[i]).attr('transform');
-            const [x1, y1] = source.substring(source.indexOf('(') + 1, source.indexOf(')')).split(',');
-            const x2 = centerX;
-            const y2 = centerY;
-            return `M${x1},${y1}L${x2},${y2}`;
-        })
-        .attr('stroke', 'rgba(255, 255, 255, 0.2)')
-        .attr('fill', 'none');
+    return progressData;
 }
 
 async function loadCourseContent() {
