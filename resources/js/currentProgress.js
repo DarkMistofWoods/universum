631ca1d3b179974aa302b97ce2d32aef2dcac384
darkmistofwoods <zetleader@gmail.com>
2024-04-24 20:07:01 -0700

    Update currentProgress.js

--- resources/js/currentProgress.js
+++ resources/js/currentProgress.js
@@ -92,8 +92,8 @@ function createVisualization(courseContent, userProgress) {
                     });
 
                 lessonGroup.append("circle")
-                    .attr("cx", lessonRadius * Math.cos(lessonAngle - moduleAngle / 2 - 180))
-                    .attr("cy", lessonRadius * Math.sin(lessonAngle - moduleAngle / 2 - 180))
+                    .attr("cx", lessonRadius * Math.cos(lessonAngle - moduleAngle / 2 - 220))
+                    .attr("cy", lessonRadius * Math.sin(lessonAngle - moduleAngle / 2 - 220))
                     .attr("r", 8)
                     .attr("fill", lesson.progress === 1 ? completedLessonColor : incompleteLessonColor);
             });
