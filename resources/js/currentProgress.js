4ac13e8f12953e4dca83ec3d64b8608b89fdbaa2
darkmistofwoods <zetleader@gmail.com>
2024-04-26 10:54:47 -0700

    Added animation

--- resources/js/currentProgress.js
+++ resources/js/currentProgress.js
@@ -75,119 +75,87 @@ function createVisualization(courseContent, userProgress) {
 
     // Update the visualization based on the zoom level
     function updateVisualization(zoomLevel) { 
-        svg.selectAll("*").remove(); // Clear the visualization
+        const duration = 1000; // Animation duration in milliseconds
+    
+        // Shrink the existing elements towards the center
+        svg.selectAll(".submodule-node, .lesson-node")
+            .transition()
+            .duration(duration)
+            .attr("transform", "scale(0.1)")
+            .style("opacity", 0)
+            .remove();
+    
+        // Shrink the center circle
+        svg.select("circle")
+            .transition()
+            .duration(duration)
+            .attr("r", 10);
 
-        const filteredModules = filteredProgressData.slice(0, filteredProgressData.length - zoomLevel);
- 
-        const moduleCount = filteredModules.length;
-        const moduleAngle = (2 * Math.PI) / moduleCount;
-
-        const moduleRadiusScale = d3.scaleLinear()
-            .domain([0, moduleCount - 1])
-            .range([radius * 0.3, radius * 0.7]);
-
-        filteredModules.forEach((module, moduleIndex) => {
-            const moduleRadius = moduleRadiusScale(moduleIndex);
-            const filteredSubModules = module.subModules.filter(subModule =>
-                subModule.lessons.some(lesson => lesson.progress === 1)
-            );
-            const subModuleCount = filteredSubModules.length;
-            const subModuleAngle = moduleAngle / subModuleCount;
-
-            filteredSubModules.forEach((subModule, subModuleIndex) => {
-                const startAngle = moduleIndex * moduleAngle + subModuleIndex * subModuleAngle;
-                const endAngle = startAngle + subModuleAngle;
-
-                const subModuleArc = d3.arc()
-                    .innerRadius(moduleRadius - 20)
-                    .outerRadius(moduleRadius)
-                    .cornerRadius(15)
-                    .startAngle(startAngle)
-                    .endAngle(endAngle);
-
-                const subModuleGroup = svg.append("g")
-                    .attr("class", "submodule-node");
-
-                const subModuleOpacity = subModule.lessons.every(lesson => lesson.progress === 1) ? 1 : 0.3;
-
-                subModuleGroup.append("path")
-                    .attr("d", subModuleArc)
-                    .attr("fill", submoduleArcColor)
-                    .attr("fill-opacity", subModuleOpacity);
-
-                const subModuleHitArea = subModuleGroup.append("path")
-                    .attr("d", subModuleArc)
-                    .attr("fill", "transparent")
-                    .attr("stroke", "transparent")
-                    .attr("stroke-width", 20)
-                    .on("mouseover", function (event) {
-                        const subModuleOuterRadius = moduleRadius + 5;
-                        const subModuleInnerRadius = moduleRadius - 10;
-                        const subModuleArcHover = d3.arc()
-                            .innerRadius(subModuleInnerRadius)
-                            .outerRadius(subModuleOuterRadius)
-                            .cornerRadius(15)
-                            .startAngle(startAngle)
-                            .endAngle(endAngle);
-
-                        d3.select(this.parentNode).select("path:not([stroke])") // Select the non-hit-area path
-                            .attr("d", subModuleArcHover);
-
-                        showTooltip(event, subModule, 'submodule');
-                    })
-                    .on("mouseout", function () {
-                        d3.select(this.parentNode).select("path:not([stroke])") // Select the non-hit-area path
-                            .attr("d", subModuleArc);
-
-                        hideTooltip();
-                    })
-                    .on("click", function (event) {
-                        event.stopPropagation();
-                        if (activeTooltip) {
-                            activeTooltip.style("display", "none");
-                        }
-                        showTooltip(event, subModule, 'submodule');
-                    });
-
-                subModule.lessons.forEach((lesson, lessonIndex) => {
-                    const lessonAngle = startAngle + (lessonIndex + 0.5) * (subModuleAngle / subModule.lessons.length) - Math.PI / 2;
-                    const lessonRadius = moduleRadius + 15;
-
-                    const lessonGroup = svg.append("g")
-                        .attr("class", "lesson-node");
-
-                    lessonGroup.append("line")
-                        .attr("x1", 0)
-                        .attr("y1", 0)
-                        .attr("x2", (lessonRadius + 10) * Math.cos(lessonAngle))
-                        .attr("y2", (lessonRadius + 10) * Math.sin(lessonAngle))
-                        .attr("stroke", lesson.progress === 1 ? completedLessonColor : incompleteLessonColor)
-                        .attr("stroke-opacity", lesson.progress === 1 ? lesson.quizScore : 1)
-                        .attr("stroke-width", 2);
-
-                    lessonGroup.append("circle")
-                        .attr("cx", lessonRadius * Math.cos(lessonAngle))
-                        .attr("cy", lessonRadius * Math.sin(lessonAngle))
-                        .attr("r", 10)
-                        .attr("fill", lesson.progress === 1 ? completedLessonColor : incompleteLessonColor);
-
-                        const lessonHitArea = lessonGroup.append("circle")
-                        .attr("cx", lessonRadius * Math.cos(lessonAngle))
-                        .attr("cy", lessonRadius * Math.sin(lessonAngle))
-                        .attr("r", 20)
+        setTimeout(() => {
+            svg.selectAll("*").remove(); // Clear the visualization
+    
+            const filteredModules = filteredProgressData.slice(0, filteredProgressData.length - zoomLevel);
+     
+            const moduleCount = filteredModules.length;
+            const moduleAngle = (2 * Math.PI) / moduleCount;
+    
+            const moduleRadiusScale = d3.scaleLinear()
+                .domain([0, moduleCount - 1])
+                .range([radius * 0.3, radius * 0.7]);
+    
+            filteredModules.forEach((module, moduleIndex) => {
+                const moduleRadius = moduleRadiusScale(moduleIndex);
+                const filteredSubModules = module.subModules.filter(subModule =>
+                    subModule.lessons.some(lesson => lesson.progress === 1)
+                );
+                const subModuleCount = filteredSubModules.length;
+                const subModuleAngle = moduleAngle / subModuleCount;
+    
+                filteredSubModules.forEach((subModule, subModuleIndex) => {
+                    const startAngle = moduleIndex * moduleAngle + subModuleIndex * subModuleAngle;
+                    const endAngle = startAngle + subModuleAngle;
+    
+                    const subModuleArc = d3.arc()
+                        .innerRadius(moduleRadius - 20)
+                        .outerRadius(moduleRadius)
+                        .cornerRadius(15)
+                        .startAngle(startAngle)
+                        .endAngle(endAngle);
+    
+                    const subModuleGroup = svg.append("g")
+                        .attr("class", "submodule-node");
+    
+                    const subModuleOpacity = subModule.lessons.every(lesson => lesson.progress === 1) ? 1 : 0.3;
+    
+                    subModuleGroup.append("path")
+                        .attr("d", subModuleArc)
+                        .attr("fill", submoduleArcColor)
+                        .attr("fill-opacity", subModuleOpacity);
+    
+                    const subModuleHitArea = subModuleGroup.append("path")
+                        .attr("d", subModuleArc)
                         .attr("fill", "transparent")
+                        .attr("stroke", "transparent")
+                        .attr("stroke-width", 20)
                         .on("mouseover", function (event) {
-                            const lessonHoverRadius = lessonRadius + 10;
-                            d3.select(this.parentNode).select("circle:not([fill='transparent'])") // Select the non-hit-area circle
-                                .attr("cx", lessonHoverRadius * Math.cos(lessonAngle))
-                                .attr("cy", lessonHoverRadius * Math.sin(lessonAngle));
-                            showTooltip(event, lesson, 'lesson');
+                            const subModuleOuterRadius = moduleRadius + 5;
+                            const subModuleInnerRadius = moduleRadius - 10;
+                            const subModuleArcHover = d3.arc()
+                                .innerRadius(subModuleInnerRadius)
+                                .outerRadius(subModuleOuterRadius)
+                                .cornerRadius(15)
+                                .startAngle(startAngle)
+                                .endAngle(endAngle);
+    
+                            d3.select(this.parentNode).select("path:not([stroke])") // Select the non-hit-area path
+                                .attr("d", subModuleArcHover);
+    
+                            showTooltip(event, subModule, 'submodule');
                         })
                         .on("mouseout", function () {
-                            d3.select(this.parentNode).select("circle:not([fill='transparent'])") // Select the non-hit-area circle
-                                .attr("cx", lessonRadius * Math.cos(lessonAngle))
-                                .attr("cy", lessonRadius * Math.sin(lessonAngle));
-
+                            d3.select(this.parentNode).select("path:not([stroke])") // Select the non-hit-area path
+                                .attr("d", subModuleArc);
+    
                             hideTooltip();
                         })
                         .on("click", function (event) {
@@ -195,47 +163,91 @@ function createVisualization(courseContent, userProgress) {
                             if (activeTooltip) {
                                 activeTooltip.style("display", "none");
                             }
-                            showTooltip(event, lesson, 'lesson');
+                            showTooltip(event, subModule, 'submodule');
                         });
+    
+                    subModule.lessons.forEach((lesson, lessonIndex) => {
+                        const lessonAngle = startAngle + (lessonIndex + 0.5) * (subModuleAngle / subModule.lessons.length) - Math.PI / 2;
+                        const lessonRadius = moduleRadius + 15;
+    
+                        const lessonGroup = svg.append("g")
+                            .attr("class", "lesson-node");
+    
+                        lessonGroup.append("line")
+                            .attr("x1", 0)
+                            .attr("y1", 0)
+                            .attr("x2", (lessonRadius + 10) * Math.cos(lessonAngle))
+                            .attr("y2", (lessonRadius + 10) * Math.sin(lessonAngle))
+                            .attr("stroke", lesson.progress === 1 ? completedLessonColor : incompleteLessonColor)
+                            .attr("stroke-opacity", lesson.progress === 1 ? lesson.quizScore : 1)
+                            .attr("stroke-width", 2);
+    
+                        lessonGroup.append("circle")
+                            .attr("cx", lessonRadius * Math.cos(lessonAngle))
+                            .attr("cy", lessonRadius * Math.sin(lessonAngle))
+                            .attr("r", 10)
+                            .attr("fill", lesson.progress === 1 ? completedLessonColor : incompleteLessonColor);
+    
+                            const lessonHitArea = lessonGroup.append("circle")
+                            .attr("cx", lessonRadius * Math.cos(lessonAngle))
+                            .attr("cy", lessonRadius * Math.sin(lessonAngle))
+                            .attr("r", 20)
+                            .attr("fill", "transparent")
+                            .on("mouseover", function (event) {
+                                const lessonHoverRadius = lessonRadius + 10;
+                                d3.select(this.parentNode).select("circle:not([fill='transparent'])") // Select the non-hit-area circle
+                                    .attr("cx", lessonHoverRadius * Math.cos(lessonAngle))
+                                    .attr("cy", lessonHoverRadius * Math.sin(lessonAngle));
+                                showTooltip(event, lesson, 'lesson');
+                            })
+                            .on("mouseout", function () {
+                                d3.select(this.parentNode).select("circle:not([fill='transparent'])") // Select the non-hit-area circle
+                                    .attr("cx", lessonRadius * Math.cos(lessonAngle))
+                                    .attr("cy", lessonRadius * Math.sin(lessonAngle));
+    
+                                hideTooltip();
+                            })
+                            .on("click", function (event) {
+                                event.stopPropagation();
+                                if (activeTooltip) {
+                                    activeTooltip.style("display", "none");
+                                }
+                                showTooltip(event, lesson, 'lesson');
+                            });
+                    });
                 });
             });
-        });
-
-        // Show the first lesson of the first submodule of the first module if no progress
-        if (filteredProgressData.length === 0) {
-            const firstModule = progressData[0];
-            const firstSubModule = firstModule.subModules[0];
-            const firstLesson = firstSubModule.lessons[0];
     
-            const lessonGroup = svg.append("g")
-                .attr("class", "lesson-node")
-                .on("mouseover", function (event) {
-                    showTooltip(event, firstLesson, 'lesson');
-                })
-                .on("mouseout", function () {
-                    hideTooltip();
-                });
-    
-            lessonGroup.append("line")
-                .attr("x1", 0)
-                .attr("y1", 0)
-                .attr("x2", 0)
-                .attr("y2", -radius * 0.2)
-                .attr("stroke", incompleteLessonColor)
-                .attr("stroke-width", 1);
-    
-            lessonGroup.append("circle")
-                .attr("cx", 0)
-                .attr("cy", -radius * 0.2)
-                .attr("r", 8)
-                .attr("fill", incompleteLessonColor);
-        }
-
-        svg.append("circle")
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("r", 5)
-            .attr("fill", completedLessonColor);
+            // Show the first lesson of the first submodule of the first module if no progress
+            if (filteredProgressData.length === 0) {
+                const firstModule = progressData[0];
+                const firstSubModule = firstModule.subModules[0];
+                const firstLesson = firstSubModule.lessons[0];
+        
+                const lessonGroup = svg.append("g")
+                    .attr("class", "lesson-node")
+                    .on("mouseover", function (event) {
+                        showTooltip(event, firstLesson, 'lesson');
+                    })
+                    .on("mouseout", function () {
+                        hideTooltip();
+                    });
+        
+                lessonGroup.append("line")
+                    .attr("x1", 0)
+                    .attr("y1", 0)
+                    .attr("x2", 0)
+                    .attr("y2", -radius * 0.2)
+                    .attr("stroke", incompleteLessonColor)
+                    .attr("stroke-width", 1);
+        
+                lessonGroup.append("circle")
+                    .attr("cx", 0)
+                    .attr("cy", -radius * 0.2)
+                    .attr("r", 8)
+                    .attr("fill", incompleteLessonColor);
+            }
+        }, duration);
     }
 
     // Create the zoom slider
@@ -253,6 +265,12 @@ function createVisualization(courseContent, userProgress) {
 
     // Initial visualization
     updateVisualization(0);
+
+    svg.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 5)
+        .attr("fill", completedLessonColor);
 }
 
 function calculateProgress(courseContent, userProgress) {
